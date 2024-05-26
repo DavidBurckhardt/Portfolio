a4f8d0016cf76973ca7b6f8e30350ffa38c861cf
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-05-26 12:13:04 -0300

    Fix size notebook

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permitir acceso en la red local
+    port: 5173, // Puerto del servidor de desarrollo
+  },
 })
