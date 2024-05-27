757e89826835316c442e302b2e6129586101e7e1
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-05-27 14:41:58 -0300

    :rocket: Skills section started

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "./styles/Home.css";
 import "./styles/About.css";
 import "./styles/Footer.css";
 import "./styles/Proyects.css";
+import "./styles/Skills.css";
 import { moverEje, rotarEje } from './utils/MoverEjes';
 import { SocialMedia } from './components/SocialMedia';
 import logo from "../public/images/logo2.png"
@@ -180,7 +181,7 @@ export default function App() {
                             <div className='data-about'>
                                 <span className='data-text'>DAVID BURCKHARDT</span>
                                 <span className='data-text'>23 YEARS OLD</span>
-                                <span className='data-text'>MAR DEL PLATA, BSAS, ARGENTINA</span>
+                                <span className='data-text'>MDQ, BSAS, ARGENTINA</span>
                                 <span className='data-text'>+54 9 2236920132</span>
                                 <span className='data-text'>BURCK432@GMAIL.COM</span>
                             </div>
@@ -188,47 +189,46 @@ export default function App() {
                         <aside className={`rightSide ${active[1]? '' : 'about-animation-down animation-delay-right'}`}>
                             <header className='title-right'><span className='title-right-text'>ABOUT ME</span></header>
                             <article className='education'>
-                                <CardAbout><h1 className='article-title'>EDUCATION</h1></CardAbout>
+                                <div className='card-title'><CardAbout><span className='article-title'>EDUCATION</span></CardAbout></div>
                                 <div className='info-about'>
-                                    <h2 className='info-about-title'>SYSTEMS ENGINEER | 2020 - PRESENTE</h2>
+                                    <span className='info-about-title'>SYSTEMS ENGINEER | 2020 - PRESENTE</span>
                                     <p className='info-about-subtitle'>
-                                        Universidad Nacional del Centro de la Provincia de Buenos Aires - UNICEN
+                                        Universidad Nacional del Centro de la Provincia de Buenos Aires | UNICEN Promedio 9.15
                                     </p>
-                                    <p className='info-about-text'>Promedio: 9.15</p>
                                 </div>
                             </article>
                             <article className='experience'>
-                                <CardAbout><h1 className='article-title'>EXPERIENCE</h1></CardAbout>
+                                <div className='card-title'><CardAbout><span className='article-title'>EXPERIENCE</span></CardAbout></div>
                                 <div className='info-about '>
-                                    <h2 className='info-about-title'>WEB DEVELOPER | 2022 | LSA + IA </h2>
+                                    <span className='info-about-title'>WEB DEVELOPER | 2022 | LSA + IA </span>
                                     <p className='info-about-subtitle'>
                                      Member of the web development team of a social project about communication and sign language.
                                     </p>
                                 </div>
                                 <div className='info-about'>
-                                    <h2 className='info-about-title'>WEB DEVELOPER | 2022 | ONG REINVENTAR</h2>
+                                    <span className='info-about-title'>WEB DEVELOPER | 2022 | ONG REINVENTAR</span>
                                     <p className='info-about-subtitle'>
                                         Front-end development team member for a website aimed at a NGO in the city of Tandil.
                                     </p>
                                 </div>
                                 <div className='info-about'>
-                                    <h2 className='info-about-title'>STUDENT ASSISTANT | 2023 - 2024 | UNICEN </h2>
+                                    <span className='info-about-title'>STUDENT ASSISTANT | 2023 - 2024 | UNICEN </span>
                                     <p className='info-about-subtitle'>
                                         I acquired communication skills while being a teaching assistant for 'Bases de Datos 1' and 'TeorIa de la Informacion' during the academic term.
                                     </p>
                                 </div>
                             </article>
                             <article className='language'>
-                                <CardAbout><h1 className='article-title'>LANGUAGES</h1></CardAbout>
+                                <div className='card-title'><CardAbout><span className='article-title'>LANGUAGES</span></CardAbout></div>
                                 <div className='info-language'>
                                     <div className='language1'>
-                                        <h2 className='info-about-title'>INGLES</h2>
+                                        <span className='info-about-title'>INGLES</span>
                                         <p className='info-about-subtitle'>
                                             Nivel B1
                                         </p>
                                     </div>
                                     <div className='language2'>
-                                        <h2 className='info-about-title'>ESPAÑOL</h2>
+                                        <span className='info-about-title'>ESPAÑOL</span>
                                         <p className='info-about-subtitle'>
                                             NATIVO
                                         </p>
@@ -243,6 +243,15 @@ export default function App() {
 
                         </article>
                         <article className='proyects-info'></article>
+                    </section>
+                    <section className="skills">
+                        <header className={`skills-title ${active[3]? 'skills-animation-up-title' : 'skills-animation-down-title'}`}><span className='skills-title-text'>SKILLS</span></header>
+                        <article className={`hard-skills ${active[3]? 'skills-animation-up-hard' : 'skills-animation-down-hard'}`}>
+
+                        </article>
+                        <article className={`soft-skills ${active[3]? 'skills-animation-up-soft' : 'skills-animation-down-soft'}`}>
+
+                        </article>
                     </section>
             </main>
             <footer className="footer">
--- src/components/CardAbout.jsx
+++ src/components/CardAbout.jsx
@@ -3,9 +3,7 @@ import "../styles/CardAbout.css"
 export default function CardAbout({children}){
     return(
         <div className="card">
-            <div className="card_title__container">
                 {children}
-            </div>
         </div>
     )
 }
