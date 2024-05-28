b9a8f7d0ade3c66ade1d1dc537d39e823c8296ea
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-05-28 14:14:59 -0300

    Responsive notebook proyects

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ export default function App() {
 
     return (
         <div className='container'>
-            <Spline scene="https://prod.spline.design/RwlJrSCvQ9Olqjib/scene.splinecode" onLoad={onLoad} />
+            {/* <Spline scene="https://prod.spline.design/RwlJrSCvQ9Olqjib/scene.splinecode" onLoad={onLoad} /> */}
             <header className='header'>
                 <section className='title'>
                     {/* <img className='logo' src={logo}/> */}
@@ -181,7 +181,7 @@ export default function App() {
             </header>
             <main className="body">
                     <section className={`home ${active[0]? 'home-animation-right' : ''}`}>
-                        <h1 className={`phrase animation-delay-phrase ${active[0]? '' : 'home-animation-left animation-delay-phrase'}`}><span className='phrase-text'>IT'S JUST A LINE OF CODE</span></h1>
+                        <h1 className={`phrase ${active[0]? '' : 'home-animation-left animation-delay-phrase'}`}><span className='phrase-text'>IT'S JUST A LINE OF CODE</span></h1>
                         <article className={`presentation ${active[0]? '' : 'home-animation-left animation-delay-presentation'}`}><span className='presentation-text'>Hi, I'm David Burckhardt, a passionate web developer and designer thrilled to welcome you to my portfolio! With a deep-rooted love for technology and design, I've embarked on an exciting journey filled with creativity and innovation. Here, you'll discover a showcase of my projects spanning my career, each reflecting my dedication to skillfulness and problem-solving. I'm eager to share my experiences and collaborate on upcoming projects, so let's connect and bring ideas to life together!</span></article>
                         <h2 className={`rol ${active[0]? '' : 'home-animation-left animation-delay-rol'}`}><span className='rol-text'>BY A SOFTWARE DEVELOPER</span></h2>
                     </section>
