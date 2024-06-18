7e3dd3bb194013fc82d5e14583d83db3d581a4f1
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-06-18 01:13:49 -0300

    Fix colors

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Portfolio",
   plugins: [react()],
   server: {
     host: true, // Permitir acceso en la red local
