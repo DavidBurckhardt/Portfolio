4ff44e69fafee6bbfd6c545bba371853cba8b1e7
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-06-18 01:25:06 -0300

    Fix colors

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -7,7 +7,8 @@ const Scene = ({ isWideScreen, onLoad }) => {
       {isWideScreen ? (
         <Spline className='scene' scene="https://prod.spline.design/Zky3yicYLH7wXaXk/scene.splinecode" onLoad={onLoad} />
       ) : (
-        <Spline scene="https://prod.spline.design/N3BgizkLX322t6wG/scene.splinecode" />
+        // <Spline scene="https://prod.spline.design/N3BgizkLX322t6wG/scene.splinecode" />
+        null
       )}
     </>
   );
