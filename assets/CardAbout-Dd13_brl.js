7200e1b7eeebd79d1a8df984ace09245e7608b0d
David <dburckhardt@alumnos.exa.unicen.edu.ar>
2024-06-22 17:14:35 -0300

    Updates

--- assets/CardAbout-Dd13_brl.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-B0hQHVSb.js";function a({children:r}){return s.jsx("div",{className:"card",children:r})}export{a as C};
---
+++ assets/CardAbout-vOnJDUGx.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DtoeOons.js";function a({children:r}){return s.jsx("div",{className:"card",children:r})}export{a as C};
